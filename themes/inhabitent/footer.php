40dc629e362246e3e11c039274753eee89df018e
Dom Yan <domyan@Doms-MacBook-Pro.local>
2019-02-26 08:44:04 -0800

    add front page headings

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -12,7 +12,6 @@
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="footer-info">
 				<div class="footer-contact-info">
-					<h3>contact info</h3>
 					<p><i class="fas fa-envelope"></i><a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('info@inhabitent.com')); ?></a></p>
                		<p><i class="fas fa-phone"></i><a href="<?php echo esc_url('#'); ?>"><?php printf(esc_html('778-456-7891')); ?></a></p>
                 	<p>
@@ -21,12 +20,7 @@
                     <span><i class="fab fa-google-plus-square"></i></span>
 					<p>
 				</div>
-				<div class="footer-business-hours">
-					<h3>business hours</h3>
-					<p><strong> Monday-Friday:</strong> 9am to 5pm</p>
-					<p><strong> Saturday:</strong> 10am to 2pm</p>
-					<p><strong> Sunday:</strong> Closed</p>
-				</div>
+				
 				<div class="footer-logo">
 					<a href= "<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg"/></a>
            		</div>
